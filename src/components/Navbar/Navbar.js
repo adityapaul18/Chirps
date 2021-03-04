3e84da933837d12d60a50f726bf3d06ea656a90b
Aditya Paul <aditya.paul18@gmail.com>
2021-03-04 15:55:49 +0530

    navbar done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import { Avatar, IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
+import AddIcon from '@material-ui/icons/Add';
 
 function Navbar() { 
     return (
@@ -16,6 +17,7 @@ function Navbar() {
             </div>
             <div className="header-right" >
             <IconButton>
+                <AddIcon className="icons" />
                 <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9Cpn5vxKV4sd6sNcK4mhsL5IibaQxPzNSg&usqp=CAU" />
             </IconButton>
             </div>
