d434cbaf16af4efc8b4503a1598b180d260fda48
Aditya Paul <aditya.paul18@gmail.com>
2021-05-05 01:43:02 +0530

    login fix ui 2

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./Login.css"
 import logo from "../../images/chirplog.png"
+import logo1 from "../../images/wlogo.jpeg"
 import {auth, provider} from '../../Firebase.js';
 import { Button } from '@material-ui/core';
-
+import logog from "../../images/Google.svg"
 
 function Login() {
     const signin = (e) => {
@@ -14,12 +15,12 @@ function Login() {
         <div className="login_container" >
             <div className="loginbox" >
                 <div>
-                <img className="logo" src={logo} alt="" />
-                    <h1 className="appheader" >Chirps-App</h1>
+                <img className="logo" id="logo12" src={logo} alt="" />
+                <img className="logo" id="logo11" src={logo1} alt="" />
                 </div>
             <div className="login_enter">
                 <p>Hop In Here</p>
-                <Button variant="contained" className="loginbutton" onClick={signin} >Login with Google</Button>
+                <Button variant="contained" className="loginbutton" onClick={signin} ><img className="glogo" src={logog} alt=""/>Login with Google</Button>
             </div>
             </div>  
         </div>
