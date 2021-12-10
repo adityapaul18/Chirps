3faab27bc0def03f0039d215ad84058065e87334
Aditya Paul <aditya.paul18@gmail.com>
2021-12-10 10:40:39 +0530

    img bg fix

--- src/components/Inputpage/Inputpage.js
+++ src/components/Inputpage/Inputpage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db, storage } from '../../Firebase'
 import './Inputpage.css'
@@ -6,20 +6,29 @@ import firebase from 'firebase'
 import { useHistory } from 'react-router'
 import FileBase from 'react-file-base64';
 import { Button, TextField } from '@material-ui/core'
+import Spinner from 'react-spinkit'
 
 function Inputpage() {
     const [chirp, setchirp] = useState("")
     const [loct, setloct] = useState("")
     const [img, setimg] = useState("")
     const [upimg, setupimg] = useState("");
+    const [bg, setbg] = useState("");
+    const [loader, setloader] = useState(0);
     const history = useHistory();
     const [user] = useAuthState(auth);
+    const ref = useRef()
+    
+    const ff = (e) => {
+        var kk= URL.createObjectURL(e.target.files[0])
+        setbg(kk)
+    }
 
     const addchirp = async (e) => {
         e.preventDefault();
 
         if (upimg) {
-
+            setloader(1)
             storage.ref(`images/${upimg.name}`).put(upimg)
                 .then(() => {
                     storage.ref('images').child(upimg.name).getDownloadURL().then(url => {
@@ -38,7 +47,9 @@ function Inputpage() {
                         setchirp("");
                         setloct("");
                         setimg("");
+                        setbg("")
                         history.push('/');
+                        setloader(0)
                     })
                 })
         }
@@ -48,7 +59,7 @@ function Inputpage() {
                 alert("cannot post empty chirp");
                 return;
             }
-            history.push('/');
+            setloader(1)
             await db.collection("messages").add({
                 user: user.displayName,
                 userpic: user.photoURL,
@@ -63,7 +74,9 @@ function Inputpage() {
             setchirp("");
             setloct("");
             setimg("");
-
+            setloader(0)
+            setbg("")
+            history.push('/');
 
         }
 
@@ -74,14 +87,23 @@ function Inputpage() {
         <>
             <div className="inputpage" >
                 <form className="inputform">
-                    <div>
-                        <div className="post_imgcont" onClick={() => { setimg("") }} style={{ backgroundImage: `url(${upimg || "https://firebase.google.com/images/social.png"})` }}></div>
-                        <TextField variant="outlined" label="Add a caption" value={chirp} onChange={(e) => setchirp(e.target.value)} />
-                        <TextField variant="outlined" label="Tag Location" value={loct} onChange={(e) => setloct(e.target.value)} />
-                        <div className="inputimager">  <input type="file" onChange={(e) => setupimg(e.target.files[0])} /> </div>
-                    </div>
-                    <div className="submit_button"><Button variant="contained" className="submitbutton" type="submit" onClick={addchirp}>Chirp</Button> </div>
-                    {/* <div className="submit_button"><Button variant="contained" className="submitbutton" type="submit" onClick={uploadimg}>Chirp</Button> </div> */}
+                    {
+                        loader ?
+                            <>
+                                <div className="loader" ><Spinner name="wandering-cubes" color="#0166C0" /></div>
+                            </> : <>
+                                <div>
+                                    <div className="post_imgcont" onClick={() => { setbg("") }} style={{ backgroundImage: `url(${bg || "https://firebase.google.com/images/social.png"})` }}></div>
+                                    <TextField variant="outlined" label="Add a caption" value={chirp} onChange={(e) => setchirp(e.target.value)} />
+                                    <TextField variant="outlined" label="Tag Location" value={loct} onChange={(e) => setloct(e.target.value)} />
+                                    <div className="inputimager" style={{ display: "none" }} >  <input ref={ref} type="file" onChange={(e) => {setupimg(e.target.files[0]); console.log(e.target);ff(e) }} /> </div>
+                                </div>
+                                <Button variant="contained" onClick={(e) => { e.preventDefault(); ref.current.click() }} className="submitbutton" type="submit" >Add image</Button>
+                                <div className="submit_button">
+                                    <Button variant="contained" className="submitbutton" type="submit" onClick={addchirp}>Chirp</Button>
+                                </div>
+                            </>
+                    }
                 </form>
             </div>
         </>
